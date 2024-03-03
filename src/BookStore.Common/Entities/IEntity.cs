f73c17d9aa1a6d87e837f50e6523fa9cb036bc78
davis <davis.nguyenduy@gmail.com>
2024-03-03 20:42:34 +0700

    rename Entity folder

--- src/BookStore.Common/Entities/IEntity.cs
+++ src/BookStore.Common/Entities/IEntity.cs
@@ -1,6 +1,12 @@
 namespace BookStore.Common.Entities
 {
     /// <summary>
+    /// A shortcut of <see cref="IEntity{TPrimaryKey}"/> for most used primary key type (<see cref="int"/>).
+    /// </summary>
+    public interface IEntity : IEntity<int>
+    {}
+
+    /// <summary>
     /// Defines interface for base entity type. All entities in the system must implement this interface.
     /// </summary>
     /// <typeparam name="TPrimaryKey">Type of the primary key of the entity</typeparam>
--- src/BookStore.Common/Entity/IEntity.cs
+++
@@ -1,8 +0,0 @@
-namespace BookStore.Common.Entities
-{
-    /// <summary>
-    /// A shortcut of <see cref="IEntity{TPrimaryKey}"/> for most used primary key type (<see cref="int"/>).
-    /// </summary>
-    public interface IEntity : IEntity<int>
-    { }
-}
